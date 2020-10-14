38fb647af8a96563e0eb78d422c5f569d1718b1f
abhigya wangoo <dnathunder@gmail.com>
2020-10-13 21:31:11 -0700

    Added tests for other features

---
+++ tests/cli_test.cc
@@ -0,0 +1,14 @@
+//
+// Created by Abhigya Wangoo on 10/13/20.
+//
+
+#include <catch2/catch.hpp>
+
+#include <core/classify.h>
+#include <fstream>
+
+TEST_CASE("sample test 1") {
+    SECTION("sample section 1") {
+        REQUIRE(true);
+    }
+}
