ff815798b6938403be8cbc0982ac009edd495aeb
abhigya wangoo <dnathunder@gmail.com>
2020-10-17 22:39:22 -0700

    Wrote framework for new tests to classify images

---
+++ tests/image_classifier_test.cc
@@ -0,0 +1,78 @@
+//
+// Created by Abhigya Wangoo on 10/17/20.
+//
+
+#include <catch2/catch.hpp>
+
+TEST_CASE("Test basic classification") {
+    SECTION("Test 3x3 classification of new image with 70% success rate") {
+        SECTION("Identify 0 correctly"){
+            
+        }
+        SECTION("Identify 1 correctly"){
+
+        }
+        SECTION("Identify 2 correctly"){
+
+        }
+        SECTION("Identify 3 correctly"){
+
+        }
+        SECTION("Identify 4 correctly"){
+
+        }
+        SECTION("Identify 5 correctly"){
+
+        }
+        SECTION("Identify 6 correctly"){
+
+        }
+        SECTION("Identify 7 correctly"){
+
+        }
+        SECTION("Identify 8 correctly"){
+
+        }
+        SECTION("Identify 9 correctly"){
+
+        }
+        
+    }
+    
+    SECTION("Test 4x4 classification of new image") {
+        SECTION("Identify 0 correctly"){
+
+        }
+        SECTION("Identify 1 correctly"){
+
+        }
+        SECTION("Identify 2 correctly"){
+
+        }
+        SECTION("Identify 3 correctly"){
+
+        }
+        SECTION("Identify 4 correctly"){
+
+        }
+        SECTION("Identify 5 correctly"){
+
+        }
+        SECTION("Identify 6 correctly"){
+
+        }
+        SECTION("Identify 7 correctly"){
+
+        }
+        SECTION("Identify 8 correctly"){
+
+        }
+        SECTION("Identify 9 correctly"){
+
+        }
+    }
+    
+    SECTION("Mathematics calculations"){
+        // TODO implement
+    }
+}
